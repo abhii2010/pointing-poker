d839092d252dca1d0ca84d9ea13ee51c7f6c7402
Shringi Abhishek <a08439@macla217264.pgds.local>
2019-09-03 12:35:43 +0530

    Pointing poker app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/:sessionId' component={Dashboard} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
 
-export default App;
---
+++ src/config/endpoint.js
@@ -0,0 +1,5 @@
+const Endpoint =  {
+  apiUrl: 'http://10.254.0.234:8080/'
+};
+
+export default Endpoint;
